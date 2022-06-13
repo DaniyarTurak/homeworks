c4510de9e297ed470606107151da6c5c9569d82e
Daniyar Turak <dany.turak@gmail.com>
2022-06-13 21:07:30 +0600

    feat: array task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,25 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = false;
 
   // Ваш код
+  if (arr1.length !== arr2.length) {
+    return result
+  }
 
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return result
+    }
+  }
+  result = true
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = [];
 
   // Ваш код
+  resultArr = arr.filter(a => a > 0 && a % 3 == 0).map(a => a * 10)
 
   return resultArr; // array
 }
