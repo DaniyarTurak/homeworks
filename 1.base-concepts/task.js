3462ca8f89b192ee53938815bd8992458debfb1f
Daniyar Turak <dany.turak@gmail.com>
2022-06-13 21:24:37 +0600

    feat: function task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,19 @@
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
+  let d = b * b - 4 * a * c;
+  if (d < 0) {
+    return arr
+
+  } else if (d == 0) {
+    let x1 = b * (-1) / (2 * a)
+    arr.push(x1);
+
+  } else {
+    var x1 = (b * (-1) + Math.sqrt(d)) / (2 * a)
+    var x2 = (b * (-1) - Math.sqrt(d)) / (2 * a)
+    arr.push(x1);
+    arr.push(x2);
+  }
   return arr; // array
 }
